424a9b950affadd06e58721d33553d21f150e5e8
naiiiden <na4ko359@gmail.com>
2023-05-28 16:30:36 +0300

    improvements

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
-let emailValue
 const form = document.querySelector('form');
 const emailInput = document.getElementById('email');
+const startContainer = document.querySelector('.start-container')
+const successContainer = document.querySelector('.success-container')
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
@@ -14,6 +15,9 @@ form.addEventListener('submit', function(event) {
   } else {
     document.querySelector('.email-error').textContent = ''
     emailInput.classList.remove('email-error-input')
+    document.querySelector('.email-span').textContent = emailInput.value
+    startContainer.style.display= 'none'
+    successContainer.style.display= 'flex'
   }
 });
 
@@ -21,3 +25,9 @@ function validateEmail(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 }
+
+document.querySelector('.dismiss-button').addEventListener('click', () => {
+    emailInput.value = ''
+    startContainer.style.display= 'grid'
+    successContainer.style.display= 'none'
+})
